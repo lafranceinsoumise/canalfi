a69b052a9eba1dce0d0a97b3d4a1a3537652b274
Guillaume Royer <perso@guilro.com>
2018-09-20 15:55:45 +0200

    get ALLOWED_HOSTS from env

--- back/canal/settings.py
+++ back/canal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9%)3r5la&rwl4v7y#4)9@7gl!y!e)q&mbv6g6$$a(e27h($bl^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
