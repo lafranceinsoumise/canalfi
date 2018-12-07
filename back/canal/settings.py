0b5f26ce35341c0f042ef6c51adf83f4bef641cf
Jill Royer <perso@guilro.com>
2018-12-07 23:06:22 +0100

    get secret from env

--- back/canal/settings.py
+++ back/canal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9%)3r5la&rwl4v7y#4)9@7gl!y!e)q&mbv6g6$$a(e27h($bl^'
+SECRET_KEY = os.environ.get("SECRET", '9%)3r5la&rwl4v7y#4)9@7gl!y!e)q&mbv6g6$$a(e27h($bl^')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'true').lower() == 'true'
